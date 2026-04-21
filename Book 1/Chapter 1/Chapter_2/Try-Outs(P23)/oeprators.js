5846ea00a3dca4c413d3c94b11123f86ad861bdf
Luis Marques <protect@i-dmarc.com>
2026-04-21 10:59:08 +0200

    Delete Book 1/Chapter 1 directory

--- Book 1/Chapter 1/Chapter_2/Try-Outs(P23)/oeprators.js
+++
@@ -1,8 +0,0 @@
-var balloons = 100;
-balloons *= 2; // This is the same as balloons = balloons * 2, so now balloons is 200
-
-// Now we want to divide the balloons by 4
-balloons /= 4; // This is the same as balloons = balloons / 4, so now balloons is 50
-
-
-
